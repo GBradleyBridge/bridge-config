2cc14fe218ec3a7ea202a7f5c3fb4b9738e5565f
Guido Accardo <guido.accardo@thebridgecorp.com>
2018-09-03 17:07:00 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='bridge-config',
+      version='0.1',
+      description='Bridge Marketing SSM simple manager',
+      url='https://github.com/BridgeMarketing/bridge-config',
+      author='Bridge',
+      author_email='guido.accardo@bridgecorp.com',
+      license='GPL',
+      long_description=open('README.rst').read(),
+      packages=find_packages(exclude=('tests')),
+      install_requires=['boto3==1.8.6'],
+      #tests_require=['mock', 'nose', 'requests-mock'],
+      zip_safe=False)
