bde609949a9d369a0f6bad012e211d50d36e6298
Guido Accardo <guido.accardo@thebridgecorp.com>
2018-09-04 13:44:05 -0300

    set version 0.8.1 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bridge-config',
-      version='0.8',
+      version='0.8.1',
       description='Bridge Marketing SSM simple manager',
       url='https://github.com/BridgeMarketing/bridge-config',
       author='Bridge',
