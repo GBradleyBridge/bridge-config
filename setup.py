9f99fec44f19de88ab50f67e204088ecd83906fa
Oscar Quintanar <oscar.quintanar@thebridgecorp.com>
2021-08-19 11:34:15 -0600

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
 
-<<<<<<< HEAD
 setup(
     name="bridge-config",
     version="1.7",
@@ -34,30 +33,3 @@ setup(
     },
     zip_safe=False,
 )
-||||||| 3f561b7
-setup(name='bridge-config',
-      version='1.4',
-      description='Bridge Marketing SSM simple manager',
-      url='https://github.com/BridgeMarketing/bridge-config',
-      author='Bridge',
-      author_email='guido.accardo@bridgecorp.com',
-      license='GPL',
-      long_description=open('README.md').read(),
-      packages=find_packages(exclude=('tests')),
-      install_requires=['boto3'],
-      #tests_require=['mock', 'nose', 'requests-mock'],
-      zip_safe=False)
-=======
-setup(name='bridge-config',
-      version='1.5',
-      description='Bridge Marketing SSM simple manager',
-      url='https://github.com/BridgeMarketing/bridge-config',
-      author='Bridge',
-      author_email='guido.accardo@bridgecorp.com',
-      license='GPL',
-      long_description=open('README.md').read(),
-      packages=find_packages(exclude=('tests')),
-      install_requires=['boto3'],
-      #tests_require=['mock', 'nose', 'requests-mock'],
-      zip_safe=False)
->>>>>>> 59c955fa82c8a095945f2550e1655cd5f44bc106
